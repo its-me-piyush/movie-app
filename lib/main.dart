8d8129ded19bc84481f4f41a1afd27aebe61d499
zsiecr <piyushnagpal777@gmail.com>
2021-10-27 19:54:07 +0530

    Update main.dart
    changed title

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter App',
+      title: 'The Movie App',
       home: HomeScreen(),
     );
   }
