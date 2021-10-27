8828f46db8a6df913a0fe55e78455b37d199c2a2
zsiecr <piyushnagpal777@gmail.com>
2021-10-27 19:51:19 +0530

    Create home_screen.dart

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+  static const routeName = '/home';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
