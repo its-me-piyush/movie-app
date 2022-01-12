5c90b52938b5bda4030fe67d99c16eef3e4bf7c5
zsiecr <piyushnagpal777@gmail.com>
2022-01-12 17:04:15 +0530

    edited constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
-const maPrimaryColor = Color(0xffd91a36);
-
 const baseUrl = 'https://api.themoviedb.org/3';
 
 const apiKey = 'dd26bd619f99177e9e20ec110207573a';
+
+const maPrimaryColor = Color(0xffd91a36);
+const maPrimaryLightColor = Color(0xFFFFECDF);
+const maSecondaryColor = Color(0xFF979797);
+const maTextColor = Colors.white;
